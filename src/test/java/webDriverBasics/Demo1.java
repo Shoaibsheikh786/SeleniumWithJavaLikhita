6591d18a00fe6747e0d60cf21a71a0e2c3bd6b7c
Muneer Ahmad <shoaib786asme@gmail.com>
2024-09-27 19:07:38 +0530

    basics done

---
+++ src/test/java/webDriverBasics/Demo1.java
@@ -0,0 +1,22 @@
+package webDriverBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+      
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://practice.expandtesting.com/");
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+		
+		String cUrl=driver.getCurrentUrl();
+		System.out.println(cUrl);
+	    String title=driver.getTitle();
+	    System.out.println(title);
+		
+	}
+
+}
